49d81bc8a1242bceb6f5f4bd73a49ec5e2fd9556
Paul Asquith <p.n.asquith@gmail.com>
2021-01-24 16:11:20 +0000

    react router added to demo app

--- src/demo/demoApp.js
+++ src/demo/demoApp.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import FlexExample from './components/FlexExample';
 import ColorExamples from './components/ColorExamples';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 
 function DemoApp() {
     return (
-        <div>
-            {/* <FlexExample /> */}
-            <ColorExamples />
-        </div>
+        <Router>
+            <div>
+                <div>
+                    <div>
+                        <Link to="/flex">Flex</Link>
+                    </div>
+                    <div>
+                        <Link to="/color">Color</Link>
+                    </div>
+                </div>
+
+                <Switch>
+                    <Route path="/flex">
+                        <FlexExample />
+                    </Route>
+                    <Route path="/color">
+                        <ColorExamples />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
