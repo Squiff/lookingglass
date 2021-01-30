7295e9c5a35911142137ad97d854b40247c1b4f3
Paul Asquith <p.n.asquith@gmail.com>
2021-01-30 10:07:37 +0000

    Typography initial commit

---
+++ src/demo/components/Typography.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function Typography() {
+    return (
+        <div className="container">
+            <h1>This is a H1 Heading</h1>
+            <h2>This is a H2 Heading</h2>
+            <h3>This is a H3 Heading</h3>
+            <h4>This is a H4 Heading</h4>
+            <h5>This is a H5 Heading</h5>
+            <h6>This is a H6 Heading</h6>
+
+            <h2>Text</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Assumenda, iusto alias veniam animi laborum ad reiciendis
+                tempora nulla, beatae possimus corporis a repellendus doloribus
+                neque tenetur autem, eveniet consectetur. Nulla natus, dolore
+                tempore autem id eum illo odit velit. Repudiandae culpa vitae
+                delectus, commodi, laboriosam rerum eius earum esse mollitia vel
+                nulla illo facere sed voluptatibus adipisci autem totam eaque
+                nam, amet quaerat reprehenderit ipsum perferendis. Nulla libero
+                quam perspiciatis, officia repellendus quod ut reiciendis sequi!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+                nulla praesentium, velit facere animi, amet quae, non omnis
+                eveniet maxime architecto suscipit laudantium officiis eius
+                molestias quibusdam eligendi maiores odio similique quaerat
+                laborum cum quam minima tenetur. Ab, consectetur corrupti
+                inventore delectus quaerat sapiente fugiat iusto sit ex rerum
+                facere.
+            </p>
+        </div>
+    );
+}
+
+export default Typography;
--- src/demo/demoApp.js
+++ src/demo/demoApp.js
@@ -4,6 +4,7 @@ import ColorExamples from './components/ColorExamples';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import AlertExamples from './components/AlertExamples';
 import ButtonExample from './components/ButtonExample';
+import Typography from './components/Typography';
 
 function DemoApp() {
     return (
@@ -22,6 +23,9 @@ function DemoApp() {
                     <div>
                         <Link to="/button">Button</Link>
                     </div>
+                    <div>
+                        <Link to="/typography">Typography</Link>
+                    </div>
                 </div>
 
                 <Switch>
@@ -36,6 +40,9 @@ function DemoApp() {
                     </Route>
                     <Route path="/button">
                         <ButtonExample />
+                    </Route>
+                    <Route path="/typography">
+                        <Typography />
                     </Route>
                 </Switch>
             </div>
