1a33a7453a9fcb2ff0012a7d6f775cd9216ff71c
Paul Asquith <p.n.asquith@gmail.com>
2021-02-16 20:28:23 +0000

    typography story added

--- src/demo/demoApp.js
+++ src/demo/demoApp.js
@@ -3,7 +3,6 @@ import FlexExample from './components/FlexExample';
 import ColorExamples from './components/ColorExamples';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import AlertExamples from './components/AlertExamples';
-import Typography from './components/Typography';
 import DrawerExample from './components/DrawerExample';
 import ModalExample from './components/ModalExample';
 import SpinnerExample from './components/SpinnerExample';
@@ -27,9 +26,7 @@ function DemoApp() {
                     <div>
                         <Link to="/alert">Alert</Link>
                     </div>
-                    <div>
-                        <Link to="/typography">Typography</Link>
-                    </div>
+
                     <div>
                         <Link to="/drawers">Drawers</Link>
                     </div>
@@ -66,9 +63,7 @@ function DemoApp() {
                     <Route path="/alert">
                         <AlertExamples />
                     </Route>
-                    <Route path="/typography">
-                        <Typography />
-                    </Route>
+
                     <Route path="/drawers">
                         <DrawerExample />
                     </Route>
