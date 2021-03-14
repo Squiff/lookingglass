2f93e2e00746efecb14ea5c4d836dbc7aa5b699f
Paul Asquith <p.n.asquith@gmail.com>
2021-03-14 23:17:05 +0000

    color stories added

--- src/stories/utilities/color.stories.js
+++ src/stories/utilities/color.stories.js
@@ -1,59 +1,95 @@
 import Lookingglass from '../../lib/components/Lookingglass';
+import { UpdateArgTable } from '../helpers/utils';
+import { argTypes as lookingGlassArgs } from './lookingglass.storyconfig';
 
-export default {
-    component: Lookingglass,
-    title: 'Utilities/Color',
-};
+// Note: directly exporting from a function is an issue for storybook
+const _argTypes = UpdateArgTable(lookingGlassArgs, { enable: ['backgroundColor', 'color'] });
+export const argTypes = _argTypes;
 
-export const Color = (args) => (
-    <Lookingglass {...args}>
-        <div className="p--3">The quick brown fox jumps over the lazy dog</div>
+/* ---------- Color Props -------------- */
+export const ColorProps = (args) => (
+    <Lookingglass {...args} div>
+        The quick brown fox jumps over the lazy dog
     </Lookingglass>
 );
 
-Color.args = {
+ColorProps.args = {
     backgroundColor: 'primary',
     color: 'white',
+    padding: '3',
 };
 
-// each of the theme colors
+/* ---------- Theme Colors -------------- */
 export const ThemeColors = (args) => (
     <>
-    <Lookingglass div padding="3" margin="1" backgroundColor="primary">
-        Primary
+        <Lookingglass div padding="3" margin="1" backgroundColor="primary" color="white">
+            Primary
         </Lookingglass>
-    <Lookingglass div padding="3" margin="1" backgroundColor="secondary" >
-        Secondary
+        <Lookingglass div padding="3" margin="1" backgroundColor="secondary" color="white">
+            Secondary
         </Lookingglass>
-    <Lookingglass div padding="3" margin="1" backgroundColor="success" >Success</Lookingglass>
-    <Lookingglass div padding="3" margin="1" backgroundColor="errror" >Error</Lookingglass>
-    <Lookingglass div padding="3" margin="1" backgroundColor="warning" >Warning</Lookingglass>
-    <Lookingglass div padding="3" margin="1" backgroundColor="info" >Info</Lookingglass>
+        <Lookingglass div padding="3" margin="1" backgroundColor="success">
+            Success
+        </Lookingglass>
+        <Lookingglass div padding="3" margin="1" backgroundColor="error">
+            Error
+        </Lookingglass>
+        <Lookingglass div padding="3" margin="1" backgroundColor="warning">
+            Warning
+        </Lookingglass>
+        <Lookingglass div padding="3" margin="1" backgroundColor="info">
+            Info
+        </Lookingglass>
     </>
-)
+);
 
-// Additional Colors
+/* ---------- Additional Colors -------------- */
 export const AdditionalColors = (args) => (
     <>
-        <Lookingglass div padding="3" margin="1" backgroundColor="white" >White</Lookingglass>
-        <Lookingglass div padding="3" margin="1" backgroundColor="black" >Black</Lookingglass>
-        <Lookingglass div padding="3" margin="1" backgroundColor="dark" >Dark</Lookingglass>
-        <Lookingglass div padding="3" margin="1" backgroundColor="light" >Light</Lookingglass>
+        <Lookingglass div padding="3" margin="1" backgroundColor="white" color="black">
+            White
+        </Lookingglass>
+        <Lookingglass div padding="3" margin="1" backgroundColor="black" color="white">
+            Black
+        </Lookingglass>
+        <Lookingglass div padding="3" margin="1" backgroundColor="dark" color="white">
+            Dark
+        </Lookingglass>
+        <Lookingglass div padding="3" margin="1" backgroundColor="light" color="black">
+            Light
+        </Lookingglass>
     </>
-)
+);
 
-// Tints and Shades
+/* ---------- Tints and Shades -------------- */
 export const TintsAndShades = (args) => (
     <>
-        <Lookingglass div padding="3" backgroundColor="primary-l4" >Primary-l4</Lookingglass>
-        <Lookingglass div padding="3" backgroundColor="primary-l3" >Primary-l3</Lookingglass>
-        <Lookingglass div padding="3" backgroundColor="primary-l2" >Primary-l2</Lookingglass>
-        <Lookingglass div padding="3" backgroundColor="primary-l1" >Primary-l1</Lookingglass>
-        <Lookingglass div padding="3" backgroundColor="primary" >Primary</Lookingglass>
-        <Lookingglass div padding="3" backgroundColor="primary-d1" >Primary-d1</Lookingglass>
-        <Lookingglass div padding="3" backgroundColor="primary-d2" >Primary-d2</Lookingglass>
-        <Lookingglass div padding="3" backgroundColor="primary-d3" >Primary-d3</Lookingglass>
-        <Lookingglass div padding="3" backgroundColor="primary-d4" >Primary-d4</Lookingglass>
+        <Lookingglass div padding="3" backgroundColor="primary-l4" color="primary-d4">
+            Primary-l4
+        </Lookingglass>
+        <Lookingglass div padding="3" backgroundColor="primary-l3" color="primary-d4">
+            Primary-l3
+        </Lookingglass>
+        <Lookingglass div padding="3" backgroundColor="primary-l2" color="primary-d4">
+            Primary-l2
+        </Lookingglass>
+        <Lookingglass div padding="3" backgroundColor="primary-l1" color="primary-d4">
+            Primary-l1
+        </Lookingglass>
+        <Lookingglass div padding="3" backgroundColor="primary" color="white">
+            Primary
+        </Lookingglass>
+        <Lookingglass div padding="3" backgroundColor="primary-d1" color="white">
+            Primary-d1
+        </Lookingglass>
+        <Lookingglass div padding="3" backgroundColor="primary-d2" color="white">
+            Primary-d2
+        </Lookingglass>
+        <Lookingglass div padding="3" backgroundColor="primary-d3" color="white">
+            Primary-d3
+        </Lookingglass>
+        <Lookingglass div padding="3" backgroundColor="primary-d4" color="white">
+            Primary-d4
+        </Lookingglass>
     </>
-
-)
+);
