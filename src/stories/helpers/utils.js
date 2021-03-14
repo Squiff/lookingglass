489f3e11bb79975ec378bc718c847d6bce39e4ab
Paul Asquith <p.n.asquith@gmail.com>
2021-03-14 23:13:47 +0000

    create lookingglass common config

--- src/stories/helpers/utils.js
+++ src/stories/helpers/utils.js
@@ -1,3 +1,5 @@
+export const themeColors = ['primary', 'secondary', 'success', 'error', 'warning', 'info'];
+
 /** remove any key where value = 'default' */
 export function cleanArgs(args, removeValue = 'default') {
     const output = { ...args };
@@ -9,4 +11,32 @@ export function cleanArgs(args, removeValue = 'default') {
     return output;
 }
 
-export const themeColors = ['primary', 'secondary', 'success', 'error', 'warning', 'info'];
+/** Disable a single Argument from the Argument Table */
+export function SetArgTableDisable(argsConfig, value) {
+    const output = { ...argsConfig };
+    output.table = { ...output.table, disable: value };
+
+    return output;
+}
+
+/**
+ * Disable/enable groups of arguments from the ArgTable
+ * @param {*} argTypes
+ * @param {{enable:[string], disable:[string], enableAll: boolean, disableAll:boolean}} options
+ */
+export function UpdateArgTable(argTypes, options) {
+    const output = { ...argTypes };
+    const { enable, enableAll, disable, disableAll } = options;
+
+    for (const k in output) {
+        if ((enable && enable.includes(k)) || enableAll) {
+            output[k] = SetArgTableDisable(output[k], false);
+        }
+
+        if ((disable && disable.includes(k)) || disableAll) {
+            output[k] = SetArgTableDisable(output[k], true);
+        }
+    }
+
+    return output;
+}
---
+++ src/stories/utilities/lookingglass.storyconfig.js
@@ -0,0 +1,401 @@
+// the 'lookingglass' utility is used by all utility stories
+// put common config here to share across stories
+
+import { UpdateArgTable } from '../helpers/utils';
+
+const allColors = [
+    'primary',
+    'secondary',
+    'success',
+    'error',
+    'warning',
+    'info',
+    'black',
+    'white',
+    'light',
+    'dark',
+    'primary-l1',
+    'primary-l2',
+    'primary-l3',
+    'primary-l4',
+    'primary-d1',
+    'primary-d2',
+    'primary-d3',
+    'primary-d4',
+];
+
+const borders = ['0', '1', '2', '3', '4'];
+
+const borderColors = [
+    'current',
+    'primary',
+    'secondary',
+    'success',
+    'error',
+    'warning',
+    'info',
+    'black',
+    'white',
+    'light',
+    'dark',
+];
+
+const borderRadius = ['0', '1', '2', '3', 'rounded'];
+
+const spacing = ['0', '1', '2', '3', '4', '5'];
+
+const widths = ['25', '50', '75', '100', '25vw', '50vw', '75vw', '100vw'];
+const heights = ['25', '50', '75', '100', '25vh', '50vh', '75vh', '100vh'];
+
+const shadows = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    '11',
+    '12',
+    '13',
+    '14',
+    '15',
+    '16',
+    '17',
+    '18',
+    '19',
+    '20',
+    '21',
+    '22',
+    '23',
+    '24',
+];
+
+const fontSizes = ['075', '0875', '1', '1125', '125', '15', '2', '25', '3'];
+
+const fontWeights = ['100', '200', '300', '400', '500', '600', '700', '800', '900'];
+const textDecorations = ['none', 'underline', 'line-through'];
+const textTransforms = ['uppercase', 'lowercase', 'capitalize'];
+const textAligns = ['left', 'right', 'center'];
+
+const display = ['none', 'block', 'inline-block'];
+
+const positions = ['fixed', 'relative', 'absolute', 'sticky'];
+const positionDistance = ['0', '100'];
+const absoluteTransforms = [
+    'top-left',
+    'top-middle',
+    'top-right',
+    'middle-left',
+    'middle',
+    'middle-right',
+    'bottom-left',
+    'bottom-middle',
+    'bottom-right',
+];
+
+const overflow = ['scroll', 'hidden', 'auto', 'visible'];
+
+const listStyleType = [
+    'none',
+    'circle',
+    'disc',
+    'square',
+    'decimal',
+    'lower-latin',
+    'upper-latin',
+    'lower-roman',
+    'upper-roman',
+];
+
+// base arg types
+const baseArgTypes = {
+    backgroundColor: {
+        control: {
+            type: 'select',
+            options: allColors,
+        },
+    },
+    color: {
+        control: {
+            type: 'select',
+            options: allColors,
+        },
+    },
+    border: {
+        control: {
+            type: 'select',
+            options: borders,
+        },
+    },
+    borderTop: {
+        control: {
+            type: 'select',
+            options: borders,
+        },
+    },
+    borderRight: {
+        control: {
+            type: 'select',
+            options: borders,
+        },
+    },
+    borderBottom: {
+        control: {
+            type: 'select',
+            options: borders,
+        },
+    },
+    borderLeft: {
+        control: {
+            type: 'select',
+            options: borders,
+        },
+    },
+    borderColor: {
+        control: {
+            type: 'select',
+            options: borderColors,
+        },
+    },
+    borderRadius: {
+        control: {
+            type: 'select',
+            options: borderRadius,
+        },
+    },
+    margin: {
+        control: {
+            type: 'select',
+            options: spacing,
+        },
+    },
+    marginTop: {
+        control: {
+            type: 'select',
+            options: spacing,
+        },
+    },
+    marginRight: {
+        control: {
+            type: 'select',
+            options: spacing,
+        },
+    },
+    marginBottom: {
+        control: {
+            type: 'select',
+            options: spacing,
+        },
+    },
+    marginLeft: {
+        control: {
+            type: 'select',
+            options: spacing,
+        },
+    },
+    marginX: {
+        control: {
+            type: 'select',
+            options: spacing,
+        },
+    },
+    marginY: {
+        control: {
+            type: 'select',
+            options: spacing,
+        },
+    },
+    padding: {
+        control: {
+            type: 'select',
+            options: spacing,
+        },
+    },
+    paddingTop: {
+        control: {
+            type: 'select',
+            options: spacing,
+        },
+    },
+    paddingRight: {
+        control: {
+            type: 'select',
+            options: spacing,
+        },
+    },
+    paddingBottom: {
+        control: {
+            type: 'select',
+            options: spacing,
+        },
+    },
+    paddingLeft: {
+        control: {
+            type: 'select',
+            options: spacing,
+        },
+    },
+    paddingX: {
+        control: {
+            type: 'select',
+            options: spacing,
+        },
+    },
+    paddingY: {
+        control: {
+            type: 'select',
+            options: spacing,
+        },
+    },
+    width: {
+        control: {
+            type: 'select',
+            options: widths,
+        },
+    },
+    height: {
+        control: {
+            type: 'select',
+            options: heights,
+        },
+    },
+    shadow: {
+        control: {
+            type: 'select',
+            options: shadows,
+        },
+    },
+    fontSize: {
+        control: {
+            type: 'select',
+            options: fontSizes,
+        },
+    },
+    fontWeight: {
+        control: {
+            type: 'select',
+            options: fontWeights,
+        },
+    },
+    textDecoration: {
+        control: {
+            type: 'select',
+            options: textDecorations,
+        },
+    },
+    textTransform: {
+        control: {
+            type: 'select',
+            options: textTransforms,
+        },
+    },
+    textAlign: {
+        control: {
+            type: 'select',
+            options: textAligns,
+        },
+    },
+    display: {
+        control: {
+            type: 'select',
+            options: display,
+        },
+    },
+    position: {
+        control: {
+            type: 'select',
+            options: positions,
+        },
+    },
+    left: {
+        control: {
+            type: 'select',
+            options: positionDistance,
+        },
+    },
+    right: {
+        control: {
+            type: 'select',
+            options: positionDistance,
+        },
+    },
+    top: {
+        control: {
+            type: 'select',
+            options: positionDistance,
+        },
+    },
+    bottom: {
+        control: {
+            type: 'select',
+            options: positionDistance,
+        },
+    },
+    absolute: {
+        control: {
+            type: 'select',
+            options: absoluteTransforms,
+        },
+    },
+    overflow: {
+        control: {
+            type: 'select',
+            options: overflow,
+        },
+    },
+    overflowX: {
+        control: {
+            type: 'select',
+            options: overflow,
+        },
+    },
+    overflowY: {
+        control: {
+            type: 'select',
+            options: overflow,
+        },
+    },
+    scrollbarColor: {
+        control: {
+            type: 'select',
+            options: ['primary', 'dark', 'light'],
+        },
+    },
+    scrollbarSize: {
+        control: {
+            type: 'radio',
+            options: ['default', 's', 'l'],
+        },
+    },
+    scrollbarStyle: {
+        control: {
+            type: 'radio',
+            options: ['default', 'none', 'rounded'],
+        },
+    },
+    scrollbarTrack: {
+        control: {
+            type: 'radio',
+            options: ['default', 'none'],
+        },
+    },
+    listStyleType: {
+        control: {
+            type: 'select',
+            options: listStyleType,
+        },
+    },
+    listPosition: {
+        control: {
+            type: 'radio',
+            options: ['default', 'inside'],
+        },
+    },
+};
+
+export const argTypes = UpdateArgTable(baseArgTypes, { disableAll: true });
