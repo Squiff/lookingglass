4a5353a22a2e3fa1271b9be1cf7d61df7cbf33e4
Paul Asquith <p.n.asquith@gmail.com>
2021-03-15 21:55:38 +0000

    position stories and docs

--- src/stories/utilities/position.stories.js
+++ src/stories/utilities/position.stories.js
@@ -1,54 +1,181 @@
 import Lookingglass from '../../lib/components/Lookingglass';
+import Button from '../../lib/components/Button';
+import Alert from '../../lib/components/Alert';
+import { cleanArgs, UpdateArgTable } from '../helpers/utils';
+import { argTypes as lookingglassArgs } from './lookingglass.storyconfig';
+import { useRef, useState } from 'react';
 
-export default {
-    component: Lookingglass,
-    title: 'Utilities/Position',
+// Note: directly exporting from a function is an issue for storybook
+const _argTypes = UpdateArgTable(lookingglassArgs, {
+    enable: ['position', 'left', 'right', 'top', 'bottom', 'absolute'],
+});
+
+export const argTypes = _argTypes;
+
+const Template = (args) => {
+    const cArgs = cleanArgs(args);
+
+    return (
+        <Lookingglass
+            position="relative"
+            backgroundColor="light"
+            width="50vw"
+            height="50vh"
+            margin="auto"
+            marginTop="5"
+        >
+            <div>
+                <Lookingglass {...cArgs} backgroundColor="dark" borderRadius="rounded">
+                    <div style={{ height: '50px', width: '50px' }}></div>
+                </Lookingglass>
+            </div>
+        </Lookingglass>
+    );
 };
 
-const Template = (args) => (
-    <Lookingglass position="relative" backgroundColor="light" width="50vw" height="50vh" margin="auto" marginTop="5">
-        <div>
-            <Lookingglass {...args} backgroundColor="dark" borderRadius="rounded">
-                <div style={{ height: '50px', width: '50px' }}></div>
+/* --------- Position Props -----------*/
+export const PositionProps = (args) => {
+    const cArgs = cleanArgs(args);
+
+    return (
+        <Lookingglass position="relative" backgroundColor="light" width="50vw" height="50vh" margin="5" div>
+            <Lookingglass
+                div
+                {...cArgs}
+                backgroundColor="dark"
+                borderRadius="rounded"
+                style={{ height: '50px', width: '50px' }}
+            />
+            <Lookingglass div absolute="middle" fontSize="15" fontWeight="600">
+                Position Relative
             </Lookingglass>
-        </div>
-    </Lookingglass>
-);
+        </Lookingglass>
+    );
+};
 
-export const Position = Template.bind({});
-
-Position.args = {
+PositionProps.args = {
     position: 'absolute',
     left: '0',
-    right: '',
-    top: '',
-    bottom: '',
 };
 
-Position.argTypes = {
-    position: { control: { type: 'select', options: ['fixed', 'relative', 'absolute', 'sticky'] } },
+/* --------- Fixed -----------*/
+export const Fixed = (args) => {
+    const [showFixed, setShowFixed] = useState(false);
+
+    return (
+        <>
+            <h6>Hide/Show Alert (Bottom Right)</h6>
+            <Button
+                color="primary"
+                onClick={() => {
+                    setShowFixed(!showFixed);
+                }}
+            >
+                Show Fixed Element
+            </Button>
+            {showFixed && (
+                <Lookingglass position="fixed" bottom="0" right="0" margin="4">
+                    <Alert color="warning">Position Fixed</Alert>
+                </Lookingglass>
+            )}
+        </>
+    );
 };
 
-export const AbsoluteTransforms = Template.bind({});
-AbsoluteTransforms.args = {
-    absolute: 'top-left',
+Fixed.parameters = {
+    docs: { source: { type: 'code' } },
 };
 
-AbsoluteTransforms.argTypes = {
-    absolute: {
-        control: {
-            type: 'select',
-            options: [
-                'top-left',
-                'top-right',
-                'top-middle',
-                'middle-left',
-                'middle-right',
-                'middle',
-                'bottom-left',
-                'bottom-right',
-                'bottom-middle',
-            ],
-        },
-    },
+/* --------- Sticky -----------*/
+export const Sticky = (args) => {
+    const containerStyles = { height: '250px', overflow: 'auto' };
+    const childStyles = { height: '750px' };
+
+    return (
+        <>
+            <div style={containerStyles}>
+                <Lookingglass position="sticky" top="0">
+                    <Alert color="error">Position Sticky</Alert>
+                </Lookingglass>
+                <div style={childStyles}>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam iure deleniti velit vitae illo
+                    amet hic assumenda tempore quaerat adipisci. Deserunt mollitia, repellendus at atque cupiditate ea?
+                    Quaerat voluptatibus, animi accusamus eveniet quibusdam voluptates maiores libero nemo laudantium?
+                    Officiis iste nemo voluptatem asperiores eum similique? Nemo amet veniam maxime optio!
+                </div>
+            </div>
+        </>
+    );
+};
+
+/* --------- Absolute -----------*/
+const Circle = () => {
+    return <Lookingglass div borderRadius="rounded" backgroundColor="dark" style={{ height: '20px', width: '20px' }} />;
+};
+
+export const Absolute = (args) => {
+    const containerStyles = { height: '100px' };
+
+    return (
+        <>
+            <Lookingglass position="relative" backgroundColor="light" margin="5" div style={containerStyles}>
+                <Lookingglass div absolute="top-left">
+                    <Circle />
+                </Lookingglass>
+                <Lookingglass div absolute="top-middle">
+                    <Circle />
+                </Lookingglass>
+                <Lookingglass div absolute="top-right">
+                    <Circle />
+                </Lookingglass>
+                <Lookingglass div absolute="middle-left">
+                    <Circle />
+                </Lookingglass>
+                <Lookingglass div absolute="middle">
+                    <Circle />
+                </Lookingglass>
+                <Lookingglass div absolute="middle-right">
+                    <Circle />
+                </Lookingglass>
+                <Lookingglass div absolute="bottom-left">
+                    <Circle />
+                </Lookingglass>
+                <Lookingglass div absolute="bottom-middle">
+                    <Circle />
+                </Lookingglass>
+                <Lookingglass div absolute="bottom-right">
+                    <Circle />
+                </Lookingglass>
+            </Lookingglass>
+        </>
+    );
+};
+
+/* --------- Absolute Button-----------*/
+export const AbsoluteButton = (args) => {
+    const notificationStyles = { height: '20px', width: '20px' };
+
+    return (
+        <Lookingglass position="relative">
+            <Button color="primary">
+                Click
+                <Lookingglass
+                    div
+                    absolute="top-right"
+                    backgroundColor="success"
+                    color="white"
+                    borderRadius="rounded"
+                    fontSize="075"
+                    fontWeight="500"
+                    border="1"
+                    borderColor="white"
+                    style={notificationStyles}
+                >
+                    <Lookingglass absolute="middle">
+                        <span>1</span>
+                    </Lookingglass>
+                </Lookingglass>
+            </Button>
+        </Lookingglass>
+    );
 };
