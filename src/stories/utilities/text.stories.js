8683c5b4d16219c6272648301a0ec867ab851dd3
Paul Asquith <p.n.asquith@gmail.com>
2021-03-16 13:02:37 +0000

    Text utilities stories and docs

--- src/stories/utilities/text.stories.js
+++ src/stories/utilities/text.stories.js
@@ -1,9 +1,13 @@
 import Lookingglass from '../../lib/components/Lookingglass';
+import { UpdateArgTable } from '../helpers/utils';
+import { argTypes as lookingglassArgs } from './lookingglass.storyconfig';
 
-export default {
-    component: Lookingglass,
-    title: 'Utilities/Text',
-};
+// Note: directly exporting from a function is an issue for storybook
+const _argTypes = UpdateArgTable(lookingglassArgs, {
+    enable: ['fontSize', 'fontWeight', 'textDecoration', 'textTransform', 'textAlign'],
+});
+
+export const argTypes = _argTypes;
 
 const Template = (args) => (
     <Lookingglass {...args}>
@@ -11,67 +15,92 @@ const Template = (args) => (
     </Lookingglass>
 );
 
+/*--------- ALL PROPS ------------ */
+export const TextProps = (args) => (
+    <Lookingglass {...args}>
+        <div>The quick brown fox jumps over the lazy dog</div>
+    </Lookingglass>
+);
+
+/*--------- ALL PROPS ------------ */
 export const FontSize = (args) => (
     <>
-        <Lookingglass fontSize="075">
-            <div>The quick brown fox jumps over the lazy dog</div>
+        <Lookingglass fontSize="0.75" div>
+            The quick brown fox jumps over the lazy dog
         </Lookingglass>
-        <Lookingglass fontSize="0875">
-            <div>The quick brown fox jumps over the lazy dog</div>
+        <Lookingglass fontSize="0.875" div>
+            The quick brown fox jumps over the lazy dog
         </Lookingglass>
-        <Lookingglass fontSize="1">
-            <div>The quick brown fox jumps over the lazy dog</div>
+        <Lookingglass fontSize="1" div>
+            The quick brown fox jumps over the lazy dog
         </Lookingglass>
-        <Lookingglass fontSize="125">
-            <div>The quick brown fox jumps over the lazy dog</div>
+        <Lookingglass fontSize="1.125" div>
+            The quick brown fox jumps over the lazy dog
         </Lookingglass>
-        <Lookingglass fontSize="15">
-            <div>The quick brown fox jumps over the lazy dog</div>
+        <Lookingglass fontSize="1.25" div>
+            The quick brown fox jumps over the lazy dog
         </Lookingglass>
-        <Lookingglass fontSize="2">
-            <div>The quick brown fox jumps over the lazy dog</div>
-        </Lookingglass>
-        <Lookingglass fontSize="25">
-            <div>The quick brown fox jumps over the lazy dog</div>
-        </Lookingglass>
-        <Lookingglass fontSize="3">
-            <div>The quick brown fox jumps over the lazy dog</div>
+        <Lookingglass fontSize="1.5" div>
+            The quick brown fox jumps over the lazy dog
         </Lookingglass>
     </>
 );
 
-export const FontWeight = Template.bind({});
+export const FontWeight = (args) => (
+    <>
+        <Lookingglass fontWeight="200" div>
+            Font Weight 200
+        </Lookingglass>
+        <Lookingglass fontWeight="400" div>
+            Font Weight 400
+        </Lookingglass>
+        <Lookingglass fontWeight="600" div>
+            Font Weight 600
+        </Lookingglass>
+        <Lookingglass fontWeight="800" div>
+            Font Weight 800
+        </Lookingglass>
+    </>
+);
 
-FontWeight.args = {
-    fontWeight: 600,
-};
+export const TextDecoration = (args) => (
+    <>
+        <Lookingglass textDecoration="none" div>
+            Text Decoration None
+        </Lookingglass>
+        <Lookingglass textDecoration="underline" div>
+            Text Decoration Underline
+        </Lookingglass>
+        <Lookingglass textDecoration="line-through" div>
+            Text Decoration Line-Through
+        </Lookingglass>
+    </>
+);
 
-export const TextDecoration = Template.bind({});
+export const TextTransform = (args) => (
+    <>
+        <Lookingglass textTransform="capitalize" div>
+            text transform capitalize
+        </Lookingglass>
+        <Lookingglass textTransform="uppercase" div>
+            Text Transform Uppercase
+        </Lookingglass>
+        <Lookingglass textTransform="lowercase" div>
+            Text Transform Lowercase
+        </Lookingglass>
+    </>
+);
 
-TextDecoration.args = {
-    textDecoration: 'underline',
-};
-
-TextDecoration.argTypes = {
-    textDecoration: { control: { type: 'select', options: ['underline', 'strike', 'none'] } },
-};
-
-export const TextTransform = Template.bind({});
-
-TextTransform.args = {
-    textTransform: 'uppercase',
-};
-
-TextTransform.argTypes = {
-    textTransform: { control: { type: 'select', options: ['capitalize', 'uppercase', 'lowercase'] } },
-};
-
-export const TextAlign = Template.bind({});
-
-TextAlign.args = {
-    textAlign: 'center',
-};
-
-TextAlign.argTypes = {
-    textAlign: { control: { type: 'select', options: ['left', 'right', 'center'] } },
-};
+export const TextAlign = (args) => (
+    <>
+        <Lookingglass textAlign="left" div>
+            Text Align Left
+        </Lookingglass>
+        <Lookingglass textAlign="center" div>
+            Text Align Center
+        </Lookingglass>
+        <Lookingglass textAlign="right" div>
+            Text Align Right
+        </Lookingglass>
+    </>
+);
