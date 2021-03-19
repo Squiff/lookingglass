177b7d589ae91af2c42c5fbec26028cc473e1911
Paul Asquith <p.n.asquith@gmail.com>
2021-03-19 21:52:35 +0000

    add spacing to story Added Progress bar Example

--- src/stories/loader.stories.js
+++ src/stories/loader.stories.js
@@ -2,16 +2,17 @@ import { useState } from 'react';
 import Loader from '../lib/components/Loader';
 import Alert from '../lib/components/Alert';
 import Button from '../lib/components/Button';
-import Spinner from '../lib/components/Spinner';
+import Progress from '../lib/components/Progress';
+import Lookingglass from '../lib/components/Lookingglass';
 
 /* -------- All Props ---------------- */
 export const AllProps = (args) => (
     <Loader {...args}>
         <Loader.Complete>
-            <h1>Status Complete</h1>
+            <Alert color="success">Complete!</Alert>
         </Loader.Complete>
         <Loader.Error>
-            <h1>Status Error</h1>
+            <Alert color="error">Error!</Alert>
         </Loader.Error>
     </Loader>
 );
@@ -20,14 +21,8 @@ AllProps.args = {
     status: 'loading',
 };
 
-/* -------- Basic ---------------- */
-export const basic = (args) => (
-    <Loader>
-        <h1>Success!</h1>
-    </Loader>
-);
-
 /* -------- With Components ---------------- */
+
 export const CustomComponents = (args) => {
     const [status, setStatus] = useState(null);
 
@@ -36,13 +31,13 @@ export const CustomComponents = (args) => {
             setStatus('loading');
             setTimeout(() => {
                 setStatus(status);
-            }, 1000);
+            }, 2000);
         }
     };
 
     return (
         <>
-            <div>
+            <Lookingglass marginRight="1" marginBottom="2">
                 <Button
                     color="success"
                     onClick={() => {
@@ -51,21 +46,22 @@ export const CustomComponents = (args) => {
                 >
                     Succeed at Something
                 </Button>
-                <Button
-                    color="error"
-                    onClick={() => {
-                        handleClick('error');
-                    }}
-                >
-                    Fail at Something
-                </Button>
-            </div>
+            </Lookingglass>
+            <Button
+                color="error"
+                onClick={() => {
+                    handleClick('error');
+                }}
+            >
+                Fail at Something
+            </Button>
+
             <Loader status={status}>
                 <Loader.Complete>
                     <Alert color="success">SUCCESS!</Alert>
                 </Loader.Complete>
                 <Loader.Loading>
-                    <Spinner size="l" />
+                    <Progress indeterminate />
                 </Loader.Loading>
                 <Loader.Error>
                     <Alert color="error">Oops!</Alert>
