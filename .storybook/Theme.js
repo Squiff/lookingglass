b98bf9c8567ce135b43f3876080551cfeee19619
Paul Asquith <p.n.asquith@gmail.com>
2021-03-22 22:48:16 +0000

    add font stack to storybook theme. Override docs header styles

---
+++ .storybook/Theme.js
@@ -0,0 +1,7 @@
+import { create } from '@storybook/theming';
+
+export default create({
+    fontBase:
+        "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans','Helvetica Neue', sans-serif",
+    textColor: '#222222',
+});
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,14 @@
+/* library styles */
 import '../src/lib/sass/index.scss';
+/* storybook specific styles */
+import './styles.css';
+
+import Theme from './Theme';
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
     // controls: { expanded: true },
+    docs: {
+        theme: Theme,
+    },
 };
