e0cc565a62ac8b257143d7e86f63657c6f3ca17c
Paul Asquith <p.n.asquith@gmail.com>
2021-04-04 15:37:57 +0100

    prevent docs inheriting dark mode from browser

--- .storybook/Theme.js
+++ .storybook/Theme.js
@@ -1,6 +1,7 @@
-import { create } from '@storybook/theming';
+import { create, themes } from '@storybook/theming';
 
 export default create({
+    base: 'light',
     fontBase:
         "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans','Helvetica Neue', sans-serif",
     textColor: '#222222',
