e750ded995cd9fc752ae5d8be8d6e24a8a8b137c
Paul Asquith <p.n.asquith@gmail.com>
2021-06-02 23:14:00 +0100

    add scrolling story

--- src/stories/tabs.stories.js
+++ src/stories/tabs.stories.js
@@ -2,54 +2,54 @@ import { useState } from 'react';
 import Tabs from '../lib/components/Tabs';
 import Button from '../lib/components/Button';
 
-
 export const argTypes = {
-    active: {control: null},
-    tabId: {control: true, 
-        description:'ID set on the Tab and Panel Subcomponents',
+    active: { control: null },
+    tabId: {
+        control: true,
+        description: 'ID set on the Tab and Panel Subcomponents',
         table: {
-        category: 'Subcomponent',
-        type: {summary: 'any'}
-    },}
+            category: 'Subcomponent',
+            type: { summary: 'any' },
+        },
+    },
+};
+
+function PanelContent({ header }) {
+    return (
+        <>
+            <h2>{header}</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quas neque aliquid
+                inventore voluptate veniam obcaecati voluptas architecto dolor magni adipisci
+                nesciunt illum fugit nostrum rerum explicabo, unde aperiam. Provident quae enim,
+                commodi adipisci iste sed totam beatae perspiciatis repudiandae exercitationem
+                fugiat rem et accusantium ut quisquam. Aspernatur, officia aut?
+            </p>
+        </>
+    );
 }
 
 /* -------- ALL PROPS ---------------- */
 export const AllProps = (args) => (
     <Tabs>
-        <Tabs.Tab tabId="1">Tab 1</Tabs.Tab>
-        <Tabs.Tab tabId="2">Tab 2</Tabs.Tab>
-        <Tabs.Tab tabId="3">Tab 3</Tabs.Tab>
-        <Tabs.Tab tabId="4">Tab 4</Tabs.Tab>
-
+        <Tabs.Tab tabId="1">Tab One</Tabs.Tab>
+        <Tabs.Tab tabId="2">Tab Two</Tabs.Tab>
+        <Tabs.Tab tabId="3">Tab Three</Tabs.Tab>
+        <Tabs.Tab tabId="4">Tab Four</Tabs.Tab>
         <Tabs.Panel tabId="1">
-            <h2>Panel 1</h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, repellendus blanditiis voluptas quaerat
-                nemo molestias ipsum vitae deleniti esse fuga voluptatem quos autem consequuntur eveniet magni
-                consequatur et enim nulla.
-            </p>
+            <PanelContent header="Panel One" />
         </Tabs.Panel>
         <Tabs.Panel tabId="2">
-            <h2>Panel 2</h2>
+            <PanelContent header="Panel Two" />
         </Tabs.Panel>
         <Tabs.Panel tabId="3">
-            <h2>Panel 3</h2>
+            <PanelContent header="Panel Three" />
         </Tabs.Panel>
         <Tabs.Panel tabId="4">
-            <h2>Panel 4</h2>
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere omnis modi quia quae fuga voluptatum
-                maxime eaque sunt, iure iusto. Molestiae quod facere aliquam doloribus dolore et tempora debitis
-                aliquid!
-            </p>
+            <PanelContent header="Panel Four" />
         </Tabs.Panel>
     </Tabs>
 );
-
-AllProps.parameters = {
-    options: {showPanel: true}
-}
-
 
 /* -------- CONTROLLED ---------------- */
 export const Controlled = (args) => {
@@ -62,26 +62,64 @@ export const Controlled = (args) => {
             <Tabs.Tab tabId="3">Tab 3</Tabs.Tab>
             <Tabs.Panel tabId="1">
                 <h5>Click Tab or the button</h5>
-                <Button onClick={() => {setActiveTab('2')}} color="primary">Go to tab 2</Button>
+                <Button
+                    onClick={() => {
+                        setActiveTab('2');
+                    }}
+                    color="primary"
+                >
+                    Go to tab 2
+                </Button>
             </Tabs.Panel>
             <Tabs.Panel tabId="2">
                 <h5>Click Tab or the button</h5>
-                <Button onClick={() => {setActiveTab('3')}} color="primary">Go to tab 3</Button>
+                <Button
+                    onClick={() => {
+                        setActiveTab('3');
+                    }}
+                    color="primary"
+                >
+                    Go to tab 3
+                </Button>
             </Tabs.Panel>
             <Tabs.Panel tabId="3">
                 <h5>Click Tab or the button</h5>
-                <Button onClick={() => {setActiveTab('1')}} color="primary">Go to tab 1</Button>
+                <Button
+                    onClick={() => {
+                        setActiveTab('1');
+                    }}
+                    color="primary"
+                >
+                    Go to tab 1
+                </Button>
             </Tabs.Panel>
         </Tabs>
     );
 };
 
 Controlled.parameters = {
-    options: {showPanel: false},
     // show hooks in source view
     docs: {
         source: {
             type: 'code',
         },
     },
+};
+
+/* -------- Scrolling ---------------- */
+export const Scrolling = (args) => {
+    return (
+        <Tabs>
+            <Tabs.Tab tabId="1">Tab Item One</Tabs.Tab>
+            <Tabs.Tab tabId="2">Tab Item Two</Tabs.Tab>
+            <Tabs.Tab tabId="3">Tab Item Three</Tabs.Tab>
+            <Tabs.Tab tabId="4">Tab Item Four</Tabs.Tab>
+            <Tabs.Tab tabId="5">Tab Item Five</Tabs.Tab>
+            <Tabs.Tab tabId="6">Tab Item Six</Tabs.Tab>
+            <Tabs.Tab tabId="7">Tab Item Seven</Tabs.Tab>
+            <Tabs.Tab tabId="8">Tab Item Eight</Tabs.Tab>
+            <Tabs.Tab tabId="9">Tab Item Nine</Tabs.Tab>
+            <Tabs.Tab tabId="10">Tab Item Ten</Tabs.Tab>
+        </Tabs>
+    );
 };
