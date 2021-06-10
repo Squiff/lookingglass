5dd272d76f445b4c73a6cb200b51c5a202faeda5
Paul Asquith <p.n.asquith@gmail.com>
2021-06-10 22:09:09 +0100

    clean up navbar stories

--- src/stories/navbar.stories.js
+++ src/stories/navbar.stories.js
@@ -7,6 +7,7 @@ import { Input } from '../lib/components/Input';
 import Label from '../lib/components/Label';
 import List from '../lib/components/List';
 import Lookingglass from '../lib/components/Lookingglass';
+import SideScroller from '../lib/components/SideScroller';
 import NavBar from '../lib/components/NavBar';
 import Card from '../lib/components/Card';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -19,37 +20,50 @@ export const parameters = {
 
 const DropdownContent = () => (
     <List>
-        <List.Button>Dropdown Item 1</List.Button>
-        <List.Button>Dropdown Item 2</List.Button>
-        <List.Button>Dropdown Item 3</List.Button>
-        <List.Button>Dropdown Item 4</List.Button>
+        <DropdownItem>Dropdown Item 1</DropdownItem>
+        <DropdownItem>Dropdown Item 2</DropdownItem>
+        <DropdownItem>Dropdown Item 3</DropdownItem>
+        <DropdownItem>Dropdown Item 4</DropdownItem>
     </List>
 );
 
+const DropdownItem = ({ children, ...props }) => (
+    <Lookingglass paddingX="3" paddingY="2" fontSize="0.875">
+        <List.Button {...props}>{children}</List.Button>
+    </Lookingglass>
+);
+
 const TrayContent = () => (
-    <Lookingglass div paddingX="4" paddingY="2" backgroundColor="primary" color="white">
+    <Lookingglass div paddingX="4" paddingY="2" paddingTop="4" marginBottom="2">
+        {/* <h6>Additional Items</h6> */}
         <Flex cols="auto">
-            <Lookingglass marginRight="4" marginBottom="2">
-                <List>
-                    <List.Item>
-                        <h6>Tray List 1</h6>
-                    </List.Item>
-                    <List.Button>Dropdown Item 1</List.Button>
-                    <List.Button>Dropdown Item 2</List.Button>
-                    <List.Button>Dropdown Item 3</List.Button>
-                    <List.Button>Dropdown Item 4</List.Button>
-                </List>
-            </Lookingglass>
             <List>
-                <List.Item>
-                    <h6>Tray List 2</h6>
-                </List.Item>
-                <List.Button>Dropdown Item 1</List.Button>
-                <List.Button>Dropdown Item 2</List.Button>
-                <List.Button>Dropdown Item 3</List.Button>
-                <List.Button>Dropdown Item 4</List.Button>
+                <TrayItem>Additional Item 1</TrayItem>
+                <TrayItem>Additional Item 2</TrayItem>
+                <TrayItem>Additional Item 3</TrayItem>
+                <TrayItem>Additional Item 4</TrayItem>
+            </List>
+
+            <List>
+                <TrayItem>Additional Item 5</TrayItem>
+                <TrayItem>Additional Item 6</TrayItem>
+                <TrayItem>Additional Item 7</TrayItem>
+                <TrayItem>Additional Item 8</TrayItem>
+            </List>
+
+            <List>
+                <TrayItem>Additional Item 9</TrayItem>
+                <TrayItem>Additional Item 10</TrayItem>
+                <TrayItem>Additional Item 11</TrayItem>
+                <TrayItem>Additional Item 12</TrayItem>
             </List>
         </Flex>
+    </Lookingglass>
+);
+
+const TrayItem = ({ children }) => (
+    <Lookingglass paddingX="4" fontSize="0.875">
+        <List.Button>{children}</List.Button>
     </Lookingglass>
 );
 
@@ -65,66 +79,63 @@ export const All = (args) => {
     const dropdownRef = useRef();
     // Tray State
     const [showTray, setShowTray] = useState(false);
-    // Drawer State
-    const [showDrawer, setShowDrawer] = useState(false);
 
     return (
         <>
-            <Lookingglass backgroundColor="primary" color="white">
-                <NavBar>
-                    <Logo />
-                    {/* wrap in a div so display: block does not interfere with Nav layout */}
-                    <Lookingglass display={{ s: 'none', l: 'block' }} div>
-                        <NavBar.Nav>
-                            <NavBar.NavLink>Home</NavBar.NavLink>
-                            <NavBar.NavLink>About</NavBar.NavLink>
-                            <NavBar.NavLink>Services</NavBar.NavLink>
-                            <NavBar.NavLink>Help</NavBar.NavLink>
-                            <NavBar.NavButton
-                                onClick={() => {
-                                    setShowDropdown(!showDropdown);
-                                }}
-                                ref={dropdownRef}
-                                aria-expanded={showDropdown}
-                                dropdown
-                            >
-                                Dropdown
-                            </NavBar.NavButton>
+            {/* <Lookingglass backgroundColor="primary" color="white"> */}
+            <NavBar>
+                <Logo />
+                <Lookingglass display={{ s: 'none', l: 'block' }} div>
+                    <NavBar.Nav>
+                        <NavBar.NavLink>Home</NavBar.NavLink>
+                        <NavBar.NavLink>About</NavBar.NavLink>
+                        <NavBar.NavLink>Services</NavBar.NavLink>
+                        <NavBar.NavLink>Help</NavBar.NavLink>
+                        <NavBar.NavButton
+                            onClick={() => {
+                                setShowDropdown(!showDropdown);
+                            }}
+                            ref={dropdownRef}
+                            aria-expanded={showDropdown}
+                            dropdown
+                        >
+                            Dropdown
+                        </NavBar.NavButton>
 
-                            <NavBar.Dropdown
-                                show={showDropdown}
-                                onClose={() => {
-                                    setShowDropdown(false);
-                                }}
-                                targetRef={dropdownRef}
-                            >
-                                <DropdownContent />
-                            </NavBar.Dropdown>
-                        </NavBar.Nav>
-                    </Lookingglass>
+                        <NavBar.Dropdown
+                            show={showDropdown}
+                            onClose={() => {
+                                setShowDropdown(false);
+                            }}
+                            targetRef={dropdownRef}
+                        >
+                            <DropdownContent />
+                        </NavBar.Dropdown>
+                    </NavBar.Nav>
+                </Lookingglass>
 
-                    <Button
-                        btnStyle="none"
-                        hoverEffect="opacity"
-                        className="navbar__btn"
-                        onClick={() => {
-                            setShowTray(!showTray);
-                        }}
-                        aria-expanded={showTray}
-                    >
-                        Open Tray
-                    </Button>
-                    <NavBar.Tray
-                        show={showTray}
-                        onClose={() => {
-                            console.log('CLOSE TRAY');
-                            setShowTray(false);
-                        }}
-                    >
-                        <TrayContent />
-                    </NavBar.Tray>
-                </NavBar>
-            </Lookingglass>
+                <Button
+                    btnStyle="none"
+                    hoverEffect="dark"
+                    className="navbar__btn"
+                    onClick={() => {
+                        setShowTray(!showTray);
+                    }}
+                    aria-expanded={showTray}
+                >
+                    Open Tray
+                </Button>
+                <NavBar.Tray
+                    show={showTray}
+                    onClose={() => {
+                        console.log('CLOSE TRAY');
+                        setShowTray(false);
+                    }}
+                >
+                    <TrayContent />
+                </NavBar.Tray>
+            </NavBar>
+            {/* </Lookingglass> */}
         </>
     );
 };
@@ -351,14 +362,14 @@ export const Dropdown = (args) => {
                 >
                     <List>
                         <List.Item>
-                            <Lookingglass margin="0">
+                            <Lookingglass margin="0" marginTop="2">
                                 <h6>Actions</h6>
                             </Lookingglass>
                         </List.Item>
                         <List.Button>Button 1</List.Button>
                         <List.Button>Button 2</List.Button>
                         <List.Item>
-                            <Lookingglass margin="0">
+                            <Lookingglass margin="0" marginTop="2">
                                 <h6>Links</h6>
                             </Lookingglass>
                         </List.Item>
@@ -411,7 +422,11 @@ export const DropdownLogin = (args) => {
                         Login
                     </Button>
                 </Lookingglass>
-                <NavBar.Dropdown show={showSignIn} onClose={() => setShowSignIn(false)} targetRef={signInRef}>
+                <NavBar.Dropdown
+                    show={showSignIn}
+                    onClose={() => setShowSignIn(false)}
+                    targetRef={signInRef}
+                >
                     <Lookingglass div paddingY="2" paddingX="2">
                         <form style={{ width: '200px' }} onSubmit={handleSubmit}>
                             <FormGroup>
@@ -452,14 +467,12 @@ DropdownLogin.parameters = {
 
 /* ---------------- Navigation Tray ---------------- */
 const TrayCard = ({ children }) => (
-    <Lookingglass padding="2" div>
-        <Lookingglass backgroundColor="primary-d2" color="white">
-            <Card>
-                <Lookingglass padding="2" fontSize="0.875" fontWeight="600">
-                    <Card.Content>{children}</Card.Content>
-                </Lookingglass>
-            </Card>
-        </Lookingglass>
+    <Lookingglass backgroundColor="primary-d2" color="white" marginX="2">
+        <Card>
+            <Lookingglass padding="2" fontSize="0.875" fontWeight="600" textAlign="center">
+                <Card.Content>{children}</Card.Content>
+            </Lookingglass>
+        </Card>
     </Lookingglass>
 );
 
@@ -488,15 +501,23 @@ export const Tray = (args) => {
                         setShowTray(false);
                     }}
                 >
-                    <Lookingglass padding="2" backgroundColor="primary">
-                        <Flex cols="auto">
-                            <TrayCard>Tray Card 1</TrayCard>
-                            <TrayCard>Tray Card 2</TrayCard>
-                            <TrayCard>Tray Card 3</TrayCard>
-                            <TrayCard>Tray Card 4</TrayCard>
-                            <TrayCard>Tray Card 5</TrayCard>
-                            <TrayCard>Tray Card 6</TrayCard>
-                        </Flex>
+                    <Lookingglass div padding="2" backgroundColor="primary">
+                        <SideScroller scrollFactor={0.4}>
+                            <Flex wrap="nowrap" cols={{ l: 8, m: 6, s: 3 }}>
+                                <TrayCard>Tray Card 1</TrayCard>
+                                <TrayCard>Tray Card 2</TrayCard>
+                                <TrayCard>Tray Card 3</TrayCard>
+                                <TrayCard>Tray Card 4</TrayCard>
+                                <TrayCard>Tray Card 5</TrayCard>
+                                <TrayCard>Tray Card 6</TrayCard>
+                                <TrayCard>Tray Card 7</TrayCard>
+                                <TrayCard>Tray Card 8</TrayCard>
+                                <TrayCard>Tray Card 9</TrayCard>
+                                <TrayCard>Tray Card 10</TrayCard>
+                                <TrayCard>Tray Card 11</TrayCard>
+                                <TrayCard>Tray Card 12</TrayCard>
+                            </Flex>
+                        </SideScroller>
                     </Lookingglass>
                 </NavBar.Tray>
             </NavBar>
