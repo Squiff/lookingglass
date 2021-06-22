6c73158a90648a75c90fb59520a88864671c8ff4
Paul Asquith <p.n.asquith@gmail.com>
2021-06-23 00:07:17 +0100

    add status icons

--- src/lib/components/icons/Info.js
+++ src/lib/components/icons/Info.js
@@ -1,17 +1,19 @@
-function ChevronRight(props) {
+import React from 'react';
+
+function Info(props) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
             height="24px"
-            width="24px"
             viewBox="0 0 24 24"
+            width="24px"
             fill="currentColor"
             {...props}
         >
             <path d="M0 0h24v24H0V0z" fill="none" />
-            <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6-6-6z" />
+            <path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
         </svg>
     );
 }
 
-export default ChevronRight;
+export default Info;
